3059940c7f8be3eba2ffd7dd06c3757aadaf403a
Nina Andrejevic <nandrejevic@alum.mit.edu>
2023-02-02 01:12:58 -0600

    Update conf.py.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,7 @@ extensions = [
 
 intersphinx_mapping = {
 	'python': ('https://docs.python.org/3/', None),
-	'pytorch": ("https://pytorch.org/docs/stable/', None),
+	'pytorch': ('https://pytorch.org/docs/stable/', None),
 	'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
 }
 intersphinx_disabled_domains = ['std']
