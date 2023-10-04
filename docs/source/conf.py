f7aef14d740b92f24b85766560115b705cb11193
ninarina12 <nandrejevic@alum.mit.edu>
2023-10-04 15:05:28 -0500

    Update configuration file.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -53,7 +53,7 @@ sys.path.insert(0, os.path.abspath('./../..'))
 
 # to display docs when using imported packages
 def setup(app):
-	import numpy, matplotlib, matplotlib.pyplot, torchdiffeq, display, utils
+	#import numpy, matplotlib, matplotlib.pyplot, torchdiffeq, display, utils
 	from dynamicxs import ode, cxs
     
 	ode.ODE.__name__ = 'ODE'
